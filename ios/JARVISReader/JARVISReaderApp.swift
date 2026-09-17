6afd2ce926d7e2f510019c525924d14585beb8fa
focustap <156120243+focustap@users.noreply.github.com>
2026-09-17 16:32:39 -0400

    Remove Shortcuts callback handling from test app

--- ios/JARVISReader/JARVISReaderApp.swift
+++ ios/JARVISReader/JARVISReaderApp.swift
@@ -21,12 +21,8 @@ struct JARVISReaderApp: App {
         WindowGroup {
             ContentView(configurationStatus: configurationStatus)
                 .onOpenURL { url in
-                    Task { @MainActor in
-                        if JARVISShortcutClient.shared.canHandle(url) {
-                            JARVISShortcutClient.shared.handleCallback(url)
-                        } else {
-                            _ = try? await Wearables.shared.handleUrl(url)
-                        }
+                    Task {
+                        _ = try? await Wearables.shared.handleUrl(url)
                     }
                 }
         }
